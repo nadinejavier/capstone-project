1b5be222141ae443f92368666b458ad1388299ef
Nadine <nad4ne@gmail.com>
2017-07-16 14:43:42 -0700

    mailboxer success/ comments success

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :event
+  validates :body, presence: true, uniqueness: true
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,19 @@ class Event < ApplicationRecord
   has_many :categories, through: :event_categories
   has_many :user_events
   has_many :users, through: :user_events
+  has_many :comments
   validates :title, presence: true
   validates :title, uniqueness: true
   validates :description, presence: true
   validates :description, length: {maximum: 5000}
   acts_as_messageable
+  
+  has_attached_file :avatar,
+                    :styles => { :medium => "150x150>", :thumb => "44x44#" },
+                    :default_url => "/images/default.png"
+
+  validates_attachment :avatar, :presence => true,
+                       :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] },
+                       :size => { :in => 0..1000.kilobytes }
+
 end
